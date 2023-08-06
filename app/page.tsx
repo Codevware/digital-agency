3d447cac6429da5fbe7f9de8a47cb0966b580377
MD. Taifur <Taifurislamashraf.bd@gmail.com>
2023-08-06 23:34:24 +0600

    some changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
-import Image from 'next/image'
 
 export default function Home() {
   return (
-   <main className="bg-background h-screen"></main>
+   <main className="bg-background h-screen">
+    Hwl
+   </main>
   )
 }
